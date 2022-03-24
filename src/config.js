e91289dc412aaaba946cb465f32a7fdea667dd19
AlexZ <zezarUS@bk.ru>
2022-03-24 19:09:59 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -47,7 +47,6 @@ module.exports = {
 
     GITHUB_URL: 'https://github.com/KeZA3D/gcgapp-manager',
     GITHUB_URL_ISSUES: 'https://github.com/KeZA3D/gcgapp-manager/issues',
-    GITHUB_URL_RAW: 'https://raw.githubusercontent.com/webtorrent/webtorrent-desktop/master',
     GITHUB_URL_RELEASES: 'https://github.com/KeZA3D/gcgapp-manager/releases',
 
     GGBOOK_DOWNLOAD_URL: 'https://ggbook.ru/downloads/ggbook/stable/ggbook.exe',
